90df7e4dfb6038853d8e054ab8208c818ed93357
Paul Snively <psnively@mac.com>
2024-01-31 10:34:20 -0500

    First, do no harm.

---
+++ src/test/java/coop/f1r3fly/fs/struct/MemoryFsTest.java
@@ -0,0 +1,46 @@
+package coop.f1r3fly.fs.struct;
+
+
+import org.junit.Test;
+import coop.f1r3fly.fs.examples.MemoryFS;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.*;
+
+public class MemoryFsTest extends BaseFsTest {
+    @Test
+    public void testReadWrite() throws Exception {
+        MemoryFS stub = new MemoryFS();
+
+        Path tmpDir = tempPath();
+        blockingMount(stub, tmpDir);
+        try {
+            File fileOne = new File(tmpDir.toAbsolutePath() + "/file1");
+            assertFalse("file mustn't exist", fileOne.exists());
+            assertTrue("file hasn't been created", fileOne.createNewFile());
+            try (PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(fileOne)))) {
+                writer.print("test1");
+                writer.print("test2");
+            }
+
+            try (Stream<String> lines = Files.lines(fileOne.toPath())) {
+                String text = lines.collect(Collectors.joining());
+                assertEquals("file content doesn't match what was written", "test1test2", text);
+            }
+
+            assertTrue("file can't be deleted", fileOne.delete());
+            assertFalse("file mustn't exist", fileOne.exists());
+        } finally {
+            stub.umount();
+        }
+    }
+
+}
