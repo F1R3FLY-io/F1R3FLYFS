aa28d27b056c1af7e2a785c3f50a8f2b6909053a
Paul Snively <psnively@mac.com>
2024-02-01 12:05:46 -0500

    First cut of ComposeContainer waiting for services to be ready. Currently fails with NPE on getMappedPort(), which is just bizarre.

--- src/test/java/coop/f1r3fly/fs/struct/F1r3flyFSTest.java
+++ src/test/java/coop/f1r3fly/fs/struct/F1r3flyFSTest.java
@@ -15,7 +15,11 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class F1r3flyFSTest {
   @Container
   public static ComposeContainer environment =
-      new ComposeContainer(new File("src/test/resources/compose-test.yml"));
+      new ComposeContainer(new File("src/test/resources/compose-test.yml"))
+      .waitingFor("boot", Wait.forListeningPorts(40401))
+      .waitingFor("validator1", Wait.forListeningPorts(50401))
+      .waitingFor("validator2", Wait.forListeningPorts(60401))
+      .waitingFor("validator3", Wait.forListeningPorts(60501));
   
   @Test
   void shouldBlah() {
