f3bfc142fdaa14dd877de679a645547cbf74d023
Paul Snively <psnively@mac.com>
2024-02-02 14:01:37 -0500

    Attempting to just run one F1r3fly node. Also failing so far, apparently due to weird withFileSystemBind issues that leave /var/lib/rnode in the container empty, and with the wrong permissions.

--- src/test/java/coop/f1r3fly/fs/struct/F1r3flyFSTest.java
+++ src/test/java/coop/f1r3fly/fs/struct/F1r3flyFSTest.java
@@ -1,11 +1,14 @@
 package coop.f1r3fly.fs.struct;
 
-import java.io.File;
+import java.time.Duration;
 
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.containers.ComposeContainer;
+import org.testcontainers.containers.BindMode;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.utility.DockerImageName;
+import org.testcontainers.images.builder.Transferable;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -13,16 +16,19 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Testcontainers
 public class F1r3flyFSTest {
+  public static final DockerImageName F1R3FLY_IMAGE = DockerImageName.parse("ghcr.io/f1r3fly-io/rnode:latest");
+  private static final Duration STARTUP_TIMEOUT = Duration.ofMinutes(1);
+
   @Container
-  public static ComposeContainer environment =
-      new ComposeContainer(new File("src/test/resources/compose-test.yml"))
-      .withExposedService("boot",       40401, Wait.forListeningPort())
-      .withExposedService("validator1", 40401, Wait.forListeningPort())
-      .withExposedService("validator2", 40401, Wait.forListeningPort())
-      .withExposedService("validator3", 40401, Wait.forListeningPort());
-  
+  static GenericContainer<?> f1r3fly = new GenericContainer<>(F1R3FLY_IMAGE)
+      .withFileSystemBind("data/", "/var/lib/rnode/", BindMode.READ_WRITE)
+      .withExposedPorts(40401)
+      .withCommand("run -s --no-upnp --allow-private-addresses --synchrony-constraint-threshold=0.0 --validator-private-key \"aebb63dc0d50e4dd29ddd94fb52103bfe0dc4941fa0c2c8a9082a191af35ffa1\"")
+      .waitingFor(Wait.forListeningPort())
+      .withStartupTimeout(STARTUP_TIMEOUT);
+
   @Test
-  void shouldBlah() {
-    assertTrue(false);
+  void shouldRunF1r3fly() {
+    assertTrue(f1r3fly.isRunning());
   }
 }
