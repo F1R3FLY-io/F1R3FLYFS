badad6cd813190692bbc1266c231722ea062a508
Andrii Stefaniv <andrii.s@devbrother.com>
2024-04-24 20:17:15 +0300

    fix typo

--- src/main/java/io/f1r3fly/fs/flags/XAttrConstants.java
+++ src/main/java/io/f1r3fly/fs/flags/XAttrConstants.java
@@ -8,7 +8,7 @@ package io.f1r3fly.fs.flags;
  * @since 05.06.15
  */
 public final class XAttrConstants {
-    public static final int XATTR_CREATE = 1;	/* set value, fail if attr already getType.  */
+    public static final int XATTR_CREATE = 1;	/* set value, fail if attr already exists.  */
     public static final int XATTR_REPLACE = 2;	/* set value, fail if attr does not exist.  */
 
     private XAttrConstants() {
